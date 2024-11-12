c10d3b2779f259d5c85b2530baecb843300b007f
kklaasen1 <kklaasen@tlu.ee>
2024-11-12 16:54:15 +0200

    feat: add page for creating flashcard set

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -2,6 +2,7 @@ import FetchDataSteps from "@/components/tutorial/fetch-data-steps"
 import { createClient } from "@/utils/supabase/server"
 import { InfoIcon } from "lucide-react"
 import { redirect } from "next/navigation"
+import Link from "next/link";
 
 export default async function HomePage() {
   const supabase = await createClient()
@@ -19,6 +20,11 @@ export default async function HomePage() {
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">see on koduleht sisseloginud kasutajatele!</h2>
         <h3 className="text-2xl mb-4">sets</h3>
+        <Link href="/create-flashcard-set">
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Create a New Flashcard Set
+          </button>
+        </Link>
       </div>
     </div>
   )
