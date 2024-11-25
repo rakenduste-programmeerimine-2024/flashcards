acc2140b1af457dec93ffef7cdcd9b336860ce43
kklaasen1 <kklaasen@tlu.ee>
2024-11-25 14:25:44 +0200

    feat: add form for flashcard sets

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/utils/supabase/server"
 import { InfoIcon } from "lucide-react"
 import { redirect } from "next/navigation"
 
+
 export default async function ProtectedPage() {
   const supabase = await createClient()
 
